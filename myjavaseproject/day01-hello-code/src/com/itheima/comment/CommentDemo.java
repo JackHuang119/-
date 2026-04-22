0a88ed31649e15cfc7882648b681d804254e23fb
黄思吉 <huangsiji@gtmsh.com>
2026-04-22 22:12:01 +0800

    创建git仓库

---
+++ myjavaseproject/day01-hello-code/src/com/itheima/comment/CommentDemo.java
@@ -0,0 +1,29 @@
+package com.itheima.comment;
+
+
+/**
+ * 注释示例
+ */
+public class CommentDemo {
+
+    static void main() {
+
+        // 单行注释
+        System.out.println("单行注释");
+
+        /*
+         * 多行注释
+         */
+        System.out.println("多行注释");
+
+        /**
+          文档注释
+         */
+        System.out.println("文档注释");
+    }
+
+
+
+
+
+}
